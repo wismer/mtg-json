d7551624137c51b1a97bd9620985ae8a87ef8b07
Matt Long <matthewhl@gmail.com>
2014-09-03 15:40:57 -0400

    more interfacing with db

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require 'sequel'
 require 'sinatra'
 require 'json'
-DB = Sequel.connect('sqlite://ftm.db')
+DB = Sequel.connect('sqlite://mtg.db')
+
 class SummaryApp < Sinatra::Base; end
 
 class SummaryApp
@@ -21,8 +22,43 @@ class SummaryApp
     erb :cards
   end
 
-  post '/cards/draft/deck' do
-    deck = JSON.parse(params[:deck])
+  post '/profile' do
+    if DB[:profiles].where(name: params[:name]).count == 0
+      DB[:profiles].insert(name: params[:name]).to_s
+    else
+      "Invalid - Name Taken"
+    end
+  end
+
+  post '/cards/draft' do
+    res = request.body.gets
+    res = JSON.parse(res)
+    cards = res['cards']
+
+    cards.each do |card|
+      DB[:cards].insert(
+        name: card['name'],
+        mana_cost: card['manaCost'],
+        type: card['type'],
+        supertype: (card['supertypes'].join(' ') if card['supertypes']),
+        sub_type: (card['subtypes'].join(' ') if card['subtypes']),
+        color: (card['colors'].join(' ') if card['colors']),
+        text: card['text'],
+        flavor: card['flavor'],
+        power: card['power'],
+        tough: card['toughness'],
+        multi_id: card['multiverseid'],
+        image_name: card['imageName'],
+        cmc: card['cmc'],
+        profile_id: res['profile_id']
+      )
+    end
+
+    "success"
+  end
+
+  get "/cards/saved" do
+    DB[:cards].count.to_s
   end
 
   get '/occupations' do
